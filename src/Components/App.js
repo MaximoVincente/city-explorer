89591ae1448d1e36ae5181f5dd347372e2cdd300
Maximo M VincenteMejia <maximovincente@gmail.com>
2022-09-26 23:08:26 -0400

    restructured applications with component files

---
+++ src/Components/App.js
@@ -0,0 +1,21 @@
+import '../App.css';
+import React from 'react';
+import Main from './Main';
+import Header from './Header';
+import Footer from './Footer';
+
+
+class App extends React.Component {
+ 
+  render() {
+    return(
+      <>
+        <Header />
+        <Main />
+        <Footer />
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <>
+                <div id="footerDiv">
+                    <p>&copy; 2022 Maximo Vincente Mejia</p>
+                </div>
+            </>
+        );
+    }
+}
+
+export default Footer;

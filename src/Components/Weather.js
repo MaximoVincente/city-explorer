09193b479e96effca192fb0172a7cc3fbbb18577
Maximo M VincenteMejia <maximovincente@gmail.com>
2022-09-28 13:02:54 -0400

    Did some Styling for fonts

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -12,7 +12,7 @@ class Weather extends React.Component{
                 {this.props.weatherData.map((a,b) => {
                     return(
                         <div key={b}>
-                            <h4>{arguments.date}</h4>
+                            <h4>{a.date}</h4>
                             <h5>{a.description}</h5>
                         </div>
                     )
